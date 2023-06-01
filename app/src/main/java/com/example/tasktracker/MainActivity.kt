330e7ad71ce98f299eb1647454a6832072936a26
isaacralph982@gmail.com <isaacralph982@gmail.com>
2023-06-01 15:29:25 +0300

    Initial commit

--- app/src/main/java/com/example/tasktracker/MainActivity.kt
+++ app/src/main/java/com/example/tasktracker/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.tasktracker
 
+import android.graphics.Paint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val deleteLink = findViewById<TextView>(R.id.deleteLink)
+        deleteLink.paintFlags = deleteLink.paintFlags or Paint.UNDERLINE_TEXT_FLAG
+
     }
 }
---
+++ app/src/main/java/com/example/tasktracker/navMenu.kt
@@ -0,0 +1,11 @@
+package com.example.tasktracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class navMenu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nav_menu)
+    }
+}
